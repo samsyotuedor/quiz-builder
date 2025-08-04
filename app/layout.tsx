ff18ca3331f77d012217b6ba23480e2d6aa0307a
samsyotuedor <samsyotuedor40@gmail.com>
2025-08-04 14:25:24 +0100

    remove js file for tailwind

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,9 +11,9 @@ const outfit = Outfit({
 })
 
 export const metadata: Metadata = {
-  title: "v0 App",
-  description: "Created with v0",
-  generator: "v0.dev",
+  title: "Quizile App",
+  description: "Created with quizile",
+  generator: "",
 }
 
 export default function RootLayout({
